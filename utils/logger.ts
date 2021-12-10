d06787b0348e669faed5de912f1ab2e217f3f5c2
Eason0729 <easonqq0000@gmail.com>
2021-12-10 23:16:13 +0800

    docker and lf

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,34 +1,21 @@
 import { Middleware } from "../deps.ts";
 import { green, bgRed, black, bgBrightWhite } from "../deps.ts";
 
-function format(
-  x: String | number,
-  d: number,
-  align: "left" | "mid" | "right" = "right"
-) {
+function format(x: String | number, d: number, align: "left" | "mid" | "right" = "right") {
   let spacing = " ";
   if (typeof x === "number") spacing = "0";
   const reSpacing = d - x.toString().length;
   if (align == "left") return x + spacing.repeat(reSpacing);
   else if (align == "right") return spacing.repeat(reSpacing) + x;
-  else
-    return (
-      spacing.repeat(reSpacing / 2) +
-      x +
-      spacing.repeat(Math.ceil(reSpacing / 2))
-    );
+  else return spacing.repeat(reSpacing / 2) + x + spacing.repeat(Math.ceil(reSpacing / 2));
 }
 
 export default async function logger(ctx: any, next: () => any): Promise<any> {
   let text = "";
   const start = Date.now();
-  text +=
-    format(ctx.request.method, 6, "mid") +
-    " " +
-    bgBrightWhite(black(format(new URL(ctx.request.url).pathname, 40, "left")));
+  text += format(ctx.request.method, 6, "mid") + " " + bgBrightWhite(black(format(new URL(ctx.request.url).pathname, 40, "left")));
   await next();
-  if (ctx.response.status.toString()[0] == "2")
-    text += " " + green(`${ctx.response.status}`);
+  if (ctx.response.status.toString()[0] == "2") text += " " + green(`${ctx.response.status}`);
   else text += " " + bgRed(black(`${ctx.response.status}`));
   text += ` ${format(Date.now() - start, 3)}ms`;
   console.log(text);
