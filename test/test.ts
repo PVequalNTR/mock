b3a405eb6b83a5038453af89f4e5afba9836a256
eason <easonqq0000@gmail.com>
2022-03-31 18:51:04 +0800

    register user test

--- test/test.ts
+++ test/test.ts
@@ -2,10 +2,10 @@ import { superoak } from "./deps.ts";
 import { app } from "../main.ts";
 Deno.test('Regist user "/user/register"', async () => {
   const request = await superoak(app);
-  await request.post("/user/register")
+  await request.post("/api/user/register")
     .set("Content-Type", "application/json")
     .send('{"name":"superoak","password":"123456","privilege": 0}')
-    .expect(200);
+    .expect(201);
 });
 
 // interface test {
